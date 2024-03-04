69757f2437ee221631bcdcc06f875a1713e40918
Todd <todd.griffin61@outlook.com>
2024-03-04 09:39:33 +0000

    refactored to use arrow function

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+const myFunction = () => {
   document.getElementById("navDropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = (event) => {
   if (!event.target.matches('.nav__icon')) {
     var dropdowns = document.getElementsByClassName("nav__dropdown-container");
     var i;
